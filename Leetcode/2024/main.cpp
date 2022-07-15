0121d09ffac8e0fbe0898dfc9ccb35bfd48e031a
Hamji <rlarjsgml919@naver.com>
2022-07-16 01:36:37 +0900

    2024

--- Leetcode/2024/main.cpp
+++ Leetcode/2024/main.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
+        int                         result = 0;
+        int                         maxim = 0;
+        unordered_map<char, int>    count;
         
+        for (int i = 0; i < answerKey.size(); i++){
+            count[answerKey[i]]++;
+            maxim = max(maxim, count[answerKey[i]]);
+            if (result - maxim < k)
+                result++;
+            else
+                count[answerKey[i - result]]--;
+        }
+        
+        return result;        
     }
 };
