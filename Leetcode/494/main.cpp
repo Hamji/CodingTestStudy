e0600121a1225e5ea7dd0e1cc8b957f2c544b451
Hamji <rlarjsgml919@naver.com>
2022-07-30 00:47:14 +0900

    494

--- Leetcode/494/main.cpp
+++ Leetcode/494/main.cpp
@@ -1,1 +1,20 @@
-	
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int index   = nums.size() - 1;
+        int current = 0;
+        return dp(nums, target, index, current);
+    }
+    
+    int dp(vector<int>& nums, int& target, int index, int current){
+        if (index < 0 && current == target)
+            return 1;
+        else if (index < 0)
+            return 0;
+        
+        int plus    = dp(nums, target, index - 1, current + nums[index]);
+        int minus   = dp(nums, target, index - 1, current - nums[index]);
+        
+        return plus + minus;
+    }
+};	
